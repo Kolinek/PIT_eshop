f32a36c274df2ec960e4cf6d9c40559ec93f0cb1
David Kolinek <david.kolinek2@gmail.com>
2024-03-24 13:17:13 +0100

    form handler pridavanie do db bez hashu

---
+++ include/dbh.inc.php
@@ -0,0 +1,17 @@
+<?php
+
+$dsn = "mysql:host=localhost;dbname=pit_eshop";
+$dbusername = "root";
+$dbpassword = "root";
+
+try {
+    $pdo = new PDO($dsn, $dbusername, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+
+
+
+
--- index.php
+++ index.php
@@ -1,3 +1,23 @@
-<?php
+<!DOCTYPE html>
+<html lang="sk">
 
-echo "ahoj";
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+
+<body>
+    <h1>Prihlasenie</h1>
+
+    <form action="include/formhandler.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="text" name="email" placeholder="Email">
+        <input type="text" name="password" placeholder="Password">
+        <button>Signup</button>
+    </form>
+    
+</body>
+</html>
